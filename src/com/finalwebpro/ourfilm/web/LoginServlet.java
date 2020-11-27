60d5c46ccab620e21dbf8dd3646b35d2447ac7d2
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-11-27 14:40:40 +0700

    Login/Register

--- src/com/finalwebpro/ourfilm/web/LoginServlet.java
+++ src/com/finalwebpro/ourfilm/web/LoginServlet.java
@@ -1,34 +1,29 @@
 package com.finalwebpro.ourfilm.web;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.finalwebpro.ourfilm.bean.LoginBean;
+import com.finalwebpro.ourfilm.dao.UserDao;
+
 /**
- * Servlet implementation class UserServlet
+ * Servlet implementation class LoginServlet
  */
-@WebServlet("/UserServlet")
-public class UserServlet extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UserServlet() {
+    public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -43,7 +38,20 @@ public class UserServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String username = request.getParameter("name_user");
+		String password = request.getParameter("password_user");
+		
+		LoginBean loginBean = new LoginBean();
+		loginBean.setEmail_user(username);
+		loginBean.setPassword_user(password);
+		
+		UserDao loginDao = new UserDao();
+		if (loginDao.validate(loginBean)) {
+			response.sendRedirect("mainpage.jsp");
+		}
+		else {
+			response.sendRedirect("login.jsp");
+		}
 	}
 
 }
