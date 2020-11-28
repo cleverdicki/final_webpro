43c44824ecca5b3414d54dc5d887902fb1dc86a9
Clever Dicki <48941105+cleverdicki@users.noreply.github.com>
2020-11-28 14:28:08 +0700

    add missing file

---
+++ src/com/finalwebpro/ourfilm/bean/Comment.java
@@ -0,0 +1,76 @@
+package com.finalwebpro.ourfilm.bean;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Comment {
+    protected int id;
+    protected String name_film;
+    protected String distributor_film;
+    protected String comment_film;
+    protected String date;
+    protected String millis;
+
+    public Comment(int id2, String name_film2, String distributor_film2, String comment_film2) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void User() {
+
+    }
+
+    public void User(String name_film, String distributor_film, String comment_film) {
+        super();
+        this.name_film = name_film;
+        this.distributor_film = distributor_film;
+        this.comment_film = comment_film;
+    }
+
+    public void User(int id, String name_film, String distributor_film, String comment_film) {
+        super();
+        this.id = id;
+        this.name_film = name_film;
+        this.distributor_film = distributor_film;
+        this.comment_film = comment_film;
+    }
+
+    public void main(String[] args) {
+        // src:
+        // https://ramj2ee.blogspot.com/2018/12/how-to-create-javasqltimestamp-object.html
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getname_film() {
+        return name_film;
+    }
+
+    public void setname_film(String name_film) {
+        this.name_film = name_film;
+    }
+
+    public String getEmail() {
+        return distributor_film;
+    }
+
+    public void setEmail(String distributor_film) {
+        this.distributor_film = distributor_film;
+    }
+
+    public String getCountry() {
+        return comment_film;
+    }
+
+    public void setCountry(String comment_film) {
+        this.comment_film = comment_film;
+    }
+}
--- src/com/finalwebpro/ourfilm/web/LogoutServlet.java
+++ src/com/finalwebpro/ourfilm/web/LogoutServlet.java
@@ -12,34 +12,38 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class logout
  */
 @WebServlet("/logout")
-public class logout extends HttpServlet {
+public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public logout() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public LogoutServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		HttpSession session = request.getSession();
-        session.removeAttribute("email_user");
-        response.sendRedirect("login.jsp");
+		session.removeAttribute("email_user");
+		response.sendRedirect("login.jsp");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
+
 }
